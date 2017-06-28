cd5a99ded6b3bd469385e91f33576f3295fcd565
Shin-Shan Yu <shin-shan.yu@cern.ch>
2017-06-28 07:21:24 +0000

    Upload new file

---
+++ models/Higgs_ZB_UFO/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.0.25
+# Mathematica version: 9.0 for Linux x86 (64-bit) (February 7, 2013)
+# Date: Thu 9 Apr 2015 18:09:46
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HIG = CouplingOrder(name = 'HIG',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+HIW = CouplingOrder(name = 'HIW',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
